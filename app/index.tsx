5d670440ab18cb9fa0ebf7ec1a6855846fe0a485
Yevhenii <lieon.jeka@gmail.com>
2025-05-29 17:01:48 +0200

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,34 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.view}>
+      <Text>Edit app/index.tsx to edit this screen.</Text> 
+      <Link 
+        href="/login" style={styles.navButton}
+      >
+        Login
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  navButton: {
+    width: 100, 
+    height: 25, 
+    backgroundColor: "coral",
+    borderRadius: 8, 
+    textAlign: "center"
+  }
+})
+
+
+
+
